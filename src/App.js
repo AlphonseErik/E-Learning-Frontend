84aed3c1c98a7d76f08dd32d79a4d20d8a53220a
Huỳnh Sơn Tùng <huynhsontung.dev@gmail.com>
2020-03-29 07:33:34 +0700

    Fix

--- src/App.js
+++ src/App.js
@@ -1,98 +1,26 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import HomeScreen from "./screens/homeScreen/HomeSreen";
-import LoginScreen from "./screens/loginScreen/LoginScreen";
-import { connect } from "react-redux";
-import { restConnector } from "./services";
-import { LOGIN, AMOUNT_PRODUCT, GET_PRODUCT } from "./redux/action/actionType";
-import reduxAction from "./redux/action/action";
-import NotFoundScreen from "./screens/notFound/NotFoundScreen";
-import RegisterScreen from "./screens/registerScreen/RegisterScreen";
-import ProductDetail from "./components/product/productDetail/ProductDetail";
-import CategoryDetail from "./components/category/categoryDetail/CategoryDetail";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import Payment from "./components/payment/Payment";
-import Header from "./layouts/header/Header";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = props => {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-
-  const theme = React.useMemo(
-    () =>
-      createMuiTheme({
-        palette: {
-          // type: prefersDarkMode ? 'dark' : 'light',
-        },
-        typography: {
-          fontFamily: [
-            "-apple-system",
-            "BlinkMacSystemFont",
-            '"Segoe UI"',
-            "Roboto",
-            '"Helvetica Neue"',
-            "Arial",
-            "sans-serif",
-            '"Apple Color Emoji"',
-            '"Segoe UI Emoji"',
-            '"Segoe UI Symbol"'
-          ].join(",")
-        }
-      }),
-    [prefersDarkMode]
-  );
-
-  useEffect(() => {
-    const accessToken = localStorage.getItem("accessToken");
-    const credentials = localStorage.getItem("account");
-    if (accessToken) {
-      restConnector.defaults.headers["Authorization"] = "Bearer " + accessToken;
-      props.dispatch(reduxAction(LOGIN, JSON.parse(credentials)));
-    }
-    const product = localStorage.getItem("product");
-    console.log("product", product);
-    if (product) {
-      props.dispatch(reduxAction(GET_PRODUCT, JSON.parse(product)));
-    }
-  }, []);
-
+function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <React.Fragment>
-          <CssBaseline />
-          <Container fixed>
-            <Typography
-              component="div"
-              style={{ backgroundColor: "#cfe8fc" }}
-            />
-            <Header />
-            <Switch>
-              <Route path="/home" exact component={HomeScreen} />
-              <Route path="/login" exact component={LoginScreen} />
-              <Route path="/register" exact component={RegisterScreen} />
-
-              <Route
-                path="/products/getbycategory/:categoryname"
-                component={CategoryDetail}
-              />
-              <Route path="/products/:id" component={ProductDetail} />
-              <Route path="/payments" component={Payment} />
-              <Route component={HomeScreen} />
-              <Route component={NotFoundScreen} />
-            </Switch>
-          </Container>
-        </React.Fragment>
-      </ThemeProvider>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
-const mapStateToProps = state => ({
-  amount: state.amount
-});
-
-export default connect(mapStateToProps)(App);
+export default App;
